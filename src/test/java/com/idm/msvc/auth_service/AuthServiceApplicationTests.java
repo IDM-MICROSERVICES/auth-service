52288c770f5fe84c0e4a25cb237dbf619c66b602
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2026-03-14 03:35:48 -0500

    fix: se corrigio error en pipeline de maven

--- src/test/java/com/idm/msvc/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/idm/msvc/auth_service/AuthServiceApplicationTests.java
@@ -2,9 +2,15 @@ package com.idm.msvc.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class AuthServiceApplicationTests {
+
+	@MockitoBean
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
