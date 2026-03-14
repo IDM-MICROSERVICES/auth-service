1b4ac75501a6a4289ca9837e76b02007233155c5
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2026-03-14 12:10:59 -0500

    fix: se corrigio error en test

--- src/test/java/com/idm/msvc/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/idm/msvc/auth_service/AuthServiceApplicationTests.java
@@ -1,16 +1,13 @@
 package com.idm.msvc.auth_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+@SpringBootTest
 class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 }
