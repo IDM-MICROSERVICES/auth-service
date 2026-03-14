29ba19089b3f5747486fcc3f8e056039cac0e0c4
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2026-03-14 18:11:18 -0500

    fix: se resolvio la issue de imports innecesarios en GlobalExceptionHandler

--- src/main/java/com/idm/msvc/auth_service/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/idm/msvc/auth_service/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.idm.msvc.auth_service.exceptions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.HashMap;
 import java.util.Map;
