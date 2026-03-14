88e840c887de6e72bfc8dc0872ed4c93ec5eca10
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2026-03-14 03:59:45 -0500

    fix: se reslovio un fallo de ApplicationContext en test usando configuracion de H2 y perfiles de prueba

--- src/test/java/com/idm/msvc/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/idm/msvc/auth_service/AuthServiceApplicationTests.java
@@ -2,18 +2,20 @@ package com.idm.msvc.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import javax.sql.DataSource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.jpa.hibernate.ddl-auto=none",
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"eureka.client.enabled=false"
+})
 class AuthServiceApplicationTests {
-
-	@MockitoBean
-	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
